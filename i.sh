6a38a89a996503c724db06a73455443b326ea60b
beastoin <ngocthinhdp@gmail.com>
2023-09-27 14:39:59 +0700

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 
+echo
 echo "Hey @RUNFLOW_SH here!"
+echo
 RELEASES_URL="https://github.com/docsion/rfsh/releases"
 FILE_BASENAME="runflow_sh"
 LATEST="$(curl -sf https://raw.githubusercontent.com/docsion/rfsh/main/latest)"
@@ -9,25 +11,25 @@ LATEST="$(curl -sf https://raw.githubusercontent.com/docsion/rfsh/main/latest)"
 test -z "$VERSION" && VERSION="$LATEST"
 
 test -z "$VERSION" && {
-	echo "Unable to get runflow_sh version." >&2
+	echo "[RFSH] Unable to get runflow version." >&2
+ 	echo
 	exit 1
 }
 
 TMP_DIR="$(mktemp -d)"
-# shellcheck disable=SC2064 # intentionally expands here
 trap "rm -rf \"$TMP_DIR\"" EXIT INT TERM
 OS="$(uname -s)"
 ARCH="$(uname -m)"
-# test "$ARCH" = "aarch64" && ARCH="arm64"
 TAR_FILE="${FILE_BASENAME}_${OS}_${ARCH}.tar.gz"
 
 (
 	cd "$TMP_DIR"
-	echo "Downloading runflow_sh $VERSION..."
+	echo "[RFSH] Downloading runflow $VERSION..."
+ 	echo
 	curl -sfLO "$RELEASES_URL/download/$VERSION/$TAR_FILE"
 )
 
 tar -xf "$TMP_DIR/$TAR_FILE" -C "$TMP_DIR"
 mv "$TMP_DIR/runflow_sh" ./runflow
-echo "Done" $(pwd)/runflow
-
+echo "[RFSH] Done" $(pwd)/runflow
+echo
