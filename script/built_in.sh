b416f5c4bc8473c91b786aba7c0a055a05d52717
beastoin <ngocthinhdp@gmail.com>
2023-10-11 15:12:27 +0700

    Clarify doc rf_err

--- script/built_in.sh
+++ script/built_in.sh
@@ -29,7 +29,7 @@ rf_require() {
 # Arguments:
 #   Log to show, a string.
 # Returns:
-#   Writes log to stderror, then return 1.
+#   Writes log to stderror
 #######################################
 rf_err() {
   echo "$*" >&2 # print to stderr
